141a13d8390c35f097de12f5d3ea429aa98a87ec
Gabriel Ramires <gabrielramiresdeoliveira1@gmail.com>
2023-11-17 15:10:22 -0300

    path command is working now in Windows.

--- src/commands/path.js
+++ src/commands/path.js
@@ -3,7 +3,6 @@ const { exec } = require("child_process");
 exports.run = async (toolbox, args) => {
   exec("cd " + args[1] + " && start .");
   console.log(toolbox.print.colors.green("Root folder da CLI aberto."));
-  process.kill(0);
 };
 
 exports.config = {
